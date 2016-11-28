bbe41ffafc92b2b3282915fcf9ca407282572a03
EyeZayZay <saiasandstorm@gmail.com>
2016-11-28 11:03:00 -0800

    Unit 1 Code for Isaac

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function CrossOut() {
+  if(document.getElementsByClassName('crossout').checked) {
+// Get Id of selected checkbox, implement crossout class to checkbox item
+  }
+  else {
+// Get Id of selected checkbox, remove crossout class from checkbox item
+  }
+}
